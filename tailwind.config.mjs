6981f83ecc38a615267e6949da91f1342b00ecc5
MikaelRothig <mrrothig@gmail.com>
2025-01-03 17:08:45 +0200

    Updated layout, added pages and navigation, added custom colors to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#1a1a1a',
+				'primary-lighter': '#272727',
+				'primary-darker': '#171717',
+				'text-primary': '#FAFAFA',
+				'text-secondary': '#ABABAB',
+			},
+		},
 	},
 	plugins: [],
 }
