9ef0992fbc732fd4bf3da0406d4cf4996118c4da
MikaelRothig <mrrothig@gmail.com>
2025-05-18 14:48:38 +0200

    Updated transitions

--- src/components/react/Carousel.jsx
+++ src/components/react/Carousel.jsx
@@ -26,9 +26,9 @@ const ImageCarousel = ({ images, interval = 5000 }) => {
     };
 
     return (
-        <div className="relative col-span-2 overflow-hidden h-44 rounded-lg bg-primary transition-all md:hover:outline md:hover:outline-4 outline-primary-lighter duration-200">
+        <div className="relative col-span-2 overflow-hidden h-44 rounded-lg bg-light-primary dark:bg-primary md:hover:outline md:hover:outline-4 outline-light-primary-lighter dark:outline-primary-lighter">
             <div
-                className="flex transition-transform duration-700 ease-in-out h-full w-full"
+                className="flex duration-700 ease-in-out h-full w-full"
                 style={{ transform: `translateX(-${currentIndex * 100}%)` }}
             >
                 {images.map((image, idx) => (
@@ -41,13 +41,13 @@ const ImageCarousel = ({ images, interval = 5000 }) => {
                 ))}
             </div>
 
-            <div className="absolute bottom-2 right-2 flex gap-1 rounded-lg p-1 bg-primary">
+            <div className="absolute bottom-2 right-2 flex gap-1 rounded-lg p-1 bg-light-primary dark:bg-primary">
                 {images.map((_, idx) => (
                     <button
                         key={idx}
                         onClick={() => goToIndex(idx)}
-                        className={`h-2 w-2 rounded-full transition-all duration-200 hover:scale-125 ${
-                            idx === currentIndex ? 'bg-accent' : 'bg-slate-300/10'
+                        className={`h-2 w-2 rounded-full hover:scale-125 ${
+                            idx === currentIndex ? 'bg-accent' : 'bg-light-primary-lighter-hover dark:bg-slate-300/10'
                         }`}
                     />
                 ))}
